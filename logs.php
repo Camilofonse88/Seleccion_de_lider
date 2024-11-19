2b7736235dbd9732912d632681d8024ef1642873
Camilofonse88 <andrescamilo.fonsecap8@gmail.com>
2024-11-19 00:08:25 -0500

    Delete logs.php

--- logs.php
+++
@@ -1,6 +0,0 @@
-<?php
-function registrarEvento($mensaje) {
-    $logFile = '/var/www/html/logs/vote.log'; // Cambia la ruta si es necesario
-    file_put_contents($logFile, date('Y-m-d H:i:s') . " - " . $mensaje . "\n", FILE_APPEND);
-}
-?>
